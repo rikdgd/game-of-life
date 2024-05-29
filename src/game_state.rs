65d6cbf9edc67268f6536c54ff6c171f81ff29db
Rik de Goede <rik.dgd@gmail.com>
2024-05-29 21:49:38 +0200

    Move get_surrounding_cell_locations to Cell struct. Simplify requirements of the method, now doesn't try to account for edge of screen.

--- src/game_state.rs
+++ src/game_state.rs
@@ -64,33 +64,6 @@ impl GameState {
 
         todo!()
     }
-    
-    fn get_surrounding_cell_locations(&self, cell: &Cell) -> [Option<Location>; 8] {
-        let mut location_list = Vec::new();
-        for x_pos_diff in -1..=1 {
-            for y_pos_diff in -1..=1 {
-                if x_pos_diff == 0 && y_pos_diff == 0 {
-                    continue;
-                }
-                
-                location_list.push(Location::new(
-                    cell.location.x + x_pos_diff, 
-                    cell.location.y + y_pos_diff
-                ));
-            }
-        }
-        
-        let mut location_buffer: [Option<Location>; 8] = [None; 8];
-        for (i, location) in location_list.iter().enumerate() {
-            if i > 7 {
-                panic!("There are somehow more than 8 surrounding locations...");
-            }
-            location_buffer[i] = Some(location.clone());
-        }
-
-
-        location_buffer
-    }
 }
 
 pub struct Cell {
@@ -105,11 +78,32 @@ impl Cell {
             location,
         }
     }
-    
-    pub fn get_surrounding_cells(&self) -> [Option<Cell>; 8] {
-        
 
-        todo!()
+    pub fn get_surrounding_cell_locations(&self) -> [Option<Location>; 8] {
+        let mut location_list = Vec::new();
+        for x_pos_diff in -1..=1 {
+            for y_pos_diff in -1..=1 {
+                if x_pos_diff == 0 && y_pos_diff == 0 {
+                    continue;
+                }
+
+                location_list.push(Location::new(
+                    self.location.x + x_pos_diff,
+                    self.location.y + y_pos_diff
+                ));
+            }
+        }
+
+        let mut location_buffer: [Option<Location>; 8] = [None; 8];
+        for (i, location) in location_list.iter().enumerate() {
+            if i > 7 {
+                panic!("There are somehow more than 8 surrounding locations...");
+            }
+            location_buffer[i] = Some(*location);
+        }
+
+
+        location_buffer
     }
 }
 
@@ -128,42 +122,31 @@ impl Location {
 
 #[cfg(test)]
 mod test {
-    use crate::game_state::{Cell, GameState, Location};
+    use crate::game_state::{Cell, Location};
 
     #[test]
     fn get_surrounding_cell_locations_test() {
-        // Arrange
-        const WIDTH: i32 = 500;
-        const HEIGHT: i32 = 500;
-        let state = GameState::new_blank(WIDTH as u32, HEIGHT as u32);
-        
         let cell = Cell::new(true, Location::new(20, 55));
-        let top_edge_cell = Cell::new(true, Location::new(451, 0));
-        let bottom_edge_cell = Cell::new(true, Location::new(451, HEIGHT - 1));
-        let far_left_cell = Cell::new(true, Location::new(0, 32));
-        let far_right_cell = Cell::new(true, Location::new(WIDTH - 1, 60));
-        let top_left_cell = Cell::new(true, Location::new(0, 0));
-        let bottom_right_cell = Cell::new(true, Location::new(WIDTH - 1, HEIGHT - 1));
         
         let expected_cell: [Option<Location>; 8] = [
             Some(Location::new(19, 54)),
-            Some(Location::new(20, 54)),
-            Some(Location::new(21, 54)),
             Some(Location::new(19, 55)),
-            Some(Location::new(21, 55)),
             Some(Location::new(19, 56)),
+            Some(Location::new(20, 54)),
             Some(Location::new(20, 56)),
+            Some(Location::new(21, 54)),
+            Some(Location::new(21, 55)),
             Some(Location::new(21, 56)),
         ];
         
         
         // Act
-        let cell_res = state.get_surrounding_cell_locations(&cell);
+        let cell_res = cell.get_surrounding_cell_locations();
         
         
         // Assert
         assert_eq!(cell_res, expected_cell);
         
-        todo!()
+        // todo!()
     }
 }
