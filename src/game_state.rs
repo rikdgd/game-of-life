b7bee04b70c05967356ffca9747dc2d51f0e3cb4
Rik de Goede <rik.dgd@gmail.com>
2024-05-30 01:46:53 +0200

    add 'get_surrounding_living_cells' function + some refactoring.

--- src/game_state.rs
+++ src/game_state.rs
@@ -28,7 +28,7 @@ impl GameState {
     }
     
     pub fn new_rand_filled(width: u32, height: u32, chance_alive: f64) -> Result<Self, String> {
-        if chance_alive > 1.0 || chance_alive < 0.0 {
+        if !(0.0..=1.0).contains(&chance_alive) {
             return Err("Please enter a chance from 0.0 to 1.0 (included)".to_string());
         }
 
@@ -52,10 +52,16 @@ impl GameState {
     }
 
     pub fn update(&mut self) {
-        todo!()
+        let mut updated_state: Vec<Vec<Cell>> = Vec::new();
+        
+        for cell_row in &self.cells {
+            for cell in cell_row {
+                
+            }
+        }
     }
 
-    pub fn get_cell_by_location(&self, location: &Location) -> Option<Cell> {
+    pub fn get_cell_by_location(&self, location: &Location) -> Option<&Cell> {
         // The application doesn't have pixels on negative indexes, 
         // so asking to get one on a negative location is always unsuccessful, 
         // since we map cells to pixels.
@@ -69,11 +75,51 @@ impl GameState {
         
         if let Some(cell_row) = self.cells.get(location.y as usize) {
             if let Some(cell) = cell_row.get(location.x as usize) {
-                return Some(cell.to_owned());
+                return Some(cell);
             }
         }
         
         None
+    }
+
+    fn get_surrounding_locations(location: &Location) -> [Option<Location>; 8] {
+        let mut location_list = Vec::new();
+        for x_pos_diff in -1..=1 {
+            for y_pos_diff in -1..=1 {
+                if x_pos_diff == 0 && y_pos_diff == 0 {
+                    continue;
+                }
+
+                location_list.push(Location::new(
+                    location.x + x_pos_diff,
+                    location.y + y_pos_diff
+                ));
+            }
+        }
+
+        let mut location_buffer: [Option<Location>; 8] = [None; 8];
+        for (i, location) in location_list.iter().enumerate() {
+            if i > 7 {
+                panic!("There are somehow more than 8 surrounding locations, what did I mess up...");
+            }
+            location_buffer[i] = Some(*location);
+        }
+        
+        location_buffer
+    }
+    
+    fn get_surrounding_living_cells(&self, location: &Location) -> u8 {
+        let surrounding_locations = GameState::get_surrounding_locations(location);
+        let mut alive_counter: u8 = 0;
+        
+        for location in surrounding_locations {
+            let cell = self.get_cell_by_location(&location.expect("Faulty location.")).unwrap();
+            if cell.is_alive {
+                alive_counter += 1;
+            }
+        }
+
+        alive_counter
     }
 }
 
@@ -90,37 +136,10 @@ impl Cell {
             location,
         }
     }
-
-    pub fn get_surrounding_cell_locations(&self) -> [Option<Location>; 8] {
-        let mut location_list = Vec::new();
-        for x_pos_diff in -1..=1 {
-            for y_pos_diff in -1..=1 {
-                if x_pos_diff == 0 && y_pos_diff == 0 {
-                    continue;
-                }
-
-                location_list.push(Location::new(
-                    self.location.x + x_pos_diff,
-                    self.location.y + y_pos_diff
-                ));
-            }
-        }
-
-        let mut location_buffer: [Option<Location>; 8] = [None; 8];
-        for (i, location) in location_list.iter().enumerate() {
-            if i > 7 {
-                panic!("There are somehow more than 8 surrounding locations...");
-            }
-            location_buffer[i] = Some(*location);
-        }
-
-
-        location_buffer
-    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-struct Location {
+pub struct Location {
     x: i32,
     y: i32,
 }
@@ -134,12 +153,12 @@ impl Location {
 
 #[cfg(test)]
 mod test {
-    use crate::game_state::{Cell, Location};
+    use crate::game_state::{GameState, Location};
 
     #[test]
     fn get_surrounding_cell_locations_test() {
-        let cell = Cell::new(true, Location::new(20, 55));
-        let cell_negative_pos = Cell::new(true, Location::new(-88, -1));
+        let location = Location::new(20, 55);
+        let negative_location = Location::new(-88, -1);
         
         let expected_cell: [Option<Location>; 8] = [
             Some(Location::new(19, 54)),
@@ -164,8 +183,8 @@ mod test {
         ];
         
         
-        let cell_res = cell.get_surrounding_cell_locations();
-        let cell_negative_pos_res = cell_negative_pos.get_surrounding_cell_locations();
+        let cell_res = GameState::get_surrounding_locations(&location);
+        let cell_negative_pos_res = GameState::get_surrounding_locations(&negative_location);
         
         
         assert_eq!(cell_res, expected_cell);
